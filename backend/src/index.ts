70c7a9157a759a27f7500632ecd87e70af7f951e
soumyarikbanerjee <126083869+banerjeesoumya@users.noreply.github.com>
2024-12-07 22:34:33 +0530

    Updated backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,10 +2,12 @@ import express from "express"
 import dotenv from "dotenv"
 import { userRouter } from "./routes/user";
 import { contentRouter } from "./routes/content";
+import cors from "cors"
+
+
 const app = express();
-
-
 app.use(express.json());
+app.use(cors())
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/content", contentRouter)
